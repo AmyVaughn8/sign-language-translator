2204bd1f0a126f38c5c8c02053a64ef1d06433f4
daniel ung <daniel.ung@sjsu.edu>
2024-04-17 19:20:54 -0700

    changed streamlit code

--- README.md
+++ README.md
@@ -7,8 +7,10 @@ AI4ALL Spring '24 Cohort: Group 2 Project
 1. Ensure you have Conda installed on your system.
 2. Clone the repository and navigate to the project directory.
 3. Run the following command to create the environment:  
-`conda env create -f environment.yml`
+ `conda env create -f environment.yml`
 4. Activate the env using:  
    `conda activate sign-vision`
-5. To deactivate the environment use:  
+5. Get the requirements:
+   `pip install -r requirements.txt`
+6. To deactivate the environment use:  
    `conda deactivate`
--- app.py
+++ app.py
@@ -1,48 +1,34 @@
 # app.py
 import streamlit as st
-import cv2
 from PIL import Image
 import numpy as np
-import tensorflow
 from tensorflow import keras
-from keras.models import load_model
 
-#Load the model
-model = load_model('model.h5')
+# Load the model
+model = keras.models.load_model('model.h5')
 
 st.title("Sign Language Recognition")
 
-# Start the webcam
-cap = cv2.VideoCapture(0)
+# Add file uploader to allow users to upload images
+uploaded_file = st.file_uploader("Upload an image...", type=["png", "jpg", "jpeg"])
 
 # Get the expected input shape of the model
 input_shape = model.input_shape[1:]
 
-while True:
-    ret, frame = cap.read()
-    if not ret:
-        continue
-
-    # Preprocess the image
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    img = Image.fromarray(frame)
-    img = img.resize(input_shape[:2])  # Resize the image to match the input shape
-    img = np.array(img)
-    img = img / 255.0
-    img = np.expand_dims(img, axis=0)
-    img = np.expand_dims(img, axis=-1)
-
-    # Reshape the input data to match the expected input shape
-    img = img.reshape((1, *input_shape))
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert('L')  # Convert image to grayscale
+    img = image.resize(input_shape[:2])  # Resize the image to match the input shape
+    img_array = np.array(img)
+    img_array = img_array / 255.0  # Scale image pixels to [0, 1]
+    img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+    img_array = np.expand_dims(img_array, axis=-1)  # Add single channel dimension
 
     # Make a prediction
-    prediction = model.predict(img)
+    prediction = model.predict(img_array)
 
     # Display the prediction
     predicted_class = np.argmax(prediction)
     st.write(f'Prediction: {predicted_class}')
 
-    # Display the image
-    st.image(frame, channels="GRAY")
-
-cap.release()
+    # Display the uploaded image
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
